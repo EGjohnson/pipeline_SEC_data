54386a5200e1bcb93b0d220f2e4999584b812bf8
gecko <beth.g.johnson@gmail.com>
2018-04-05 22:18:46 -0400

    created dictionary for live reads

--- src/sessionization.py
+++ src/sessionization.py
@@ -19,40 +19,45 @@ Created on Tue Apr  3 15:05:46 2018
 import os
 import time
 from datetime import datetime
+import gc
 #os.chdir('~/PetGit/pipeline_SEC_data/input') # Provide the path here
 print os.getcwd() # Prints the working directory
-datapath='/home/ej/PetGit/pipeline_SEC_data/input/log_badtime.csv'
+datapath='/home/ej/PetGit/pipeline_SEC_data/input/log.csv'
 fh = open(datapath, 'r')
 location_checked=[]
 the_split=[]
+cs={} #dictionary of current sessions
+error_rec=[]
 #functions
 #===================================================================
 check_len = lambda x: len(x)==15
-#grab_dat=lambda x: x[0:3]+x[4:7]
-
-
+no_blank=lambda x: '' not in x[0:3]+x[4:7]
+no_space=lambda x: ' ' not in x[0:3]+x[4:7]
+ip_lapsed=lambda tups,e,t: set([x[0] for x in tups if x[1]+e<t])
 
 #....................................................................
 def check_field_length(lx,i): #takes in raw line
     if len(lx)!=15 and len(lx)>0:
-        print "L"+str(i)+" check_field_length error: not enough fields!"
         return False
     else:
         return True
+    
  
 def convert_datetime(lx,i):
         dts=str(lx[1])+str(",")+str(lx[2]) #date time string
         dto=datetime.strptime(dts,'%Y-%m-%d,%H:%M:%S') #date time object
         return dto
-        #if cannot convert fields into datetime object return False
-#    except Exception as e:
-#        print  "L"+str(i)+" convert_datetime error: " +str(e)
-#        return e
+
 def convert_unique_doc_request(lx,i):
         udr=str(lx[4])+str(lx[5])+str(lx[6]) #key unique doc request
         return udr
-        
+
+
     
+    
+    #input elapsed time and tuples of live sessions
+    
+
 #................................................................
          
 #buffer past header
@@ -72,9 +77,9 @@ while True:
     line_split=line.split(',')
     check_len(line_split)
     the_split.append(line_split)
-    print '-----------'*2
-    print line_split
-    print "line split length: ", len(line_split)
+    #print '-----------'*2
+    #print line_split
+    #print "line split length: ", len(line_split)
     
     
     
@@ -82,7 +87,7 @@ while True:
     #==============================================================
     #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     if not line:
-        print "no line!"
+        error_rec.append("L"+str(i)+" no line")
         time.sleep(5) # wait five seconds
         fh.seek(try_this_line) #reset back to the same line and check again
       
@@ -102,32 +107,50 @@ while True:
     
     # 3. Otherwise process the line
     #=============================================================
-    print "begin processing line"
+    #print "begin processing line"
     
     # a. verify we do not have a truncated line, else give up
     #--------------------------------------------------------------
     if check_field_length(line_split,i)==True:
         pass
     else:
+        error_rec.append("L"+str(i)+" truncated line missing fields")
         continue
     
-    #.b attempt to create datetime object from fields or give up
+    # b. verify we do not have blank fields, else give up
+    #--------------------------------------------------------------
+    if no_blank(line_split)==True and no_space(line_split)==True:
+        pass
+    else:
+        error_rec.append("L"+str(i)+" blank or space in field")
+        continue
+    
+    # c. attempt to create datetime object from fields, else give up
     #--------------------------------------------------------------
     try:
         dto=convert_datetime(line_split,i) #make datetime object from fields
     except Exception as e:
-        print  "L"+str(i)+" convert_datetime error: " +str(e)
+        error_rec.append("L"+str(i)+" convert_datetime error: " +str(e))
+        continue
+    
+    # d. attempt to create unique doc request key, else give up
+    #--------------------------------------------------------------
+    try:
+        urk=convert_unique_doc_request(line_split,i) #unique request key
+    except Exception as e:
+        error_rec.append("L"+str(i)+" convert_unique_doc_request error: " +str(e))
         continue
     
     
+    # 4. Now add to dictionary object of current sessions
+    #------------------------------------------------------------------
+    #ip address, time of 1st request, time of 2nd request, duration, count requests
+    cs[line_split[0]+str(i)]=(dto,line_split[0],urk+str(i)) # add datetime and ip and access record
+    #session  time1stdoc-timelastdoc+elapsedtime
+    #duration time1stdoc-timelastdoc
+    
     
  
- 
+gc.collect() 
 fh.seek(0)   #reset
-    
-    # b. verify we can generate a dattime object from fields
-
-#    if len(line_split)!=15 and len(line_split)>0:
-#        print "entry too short!"
-
     
--- src/test_sessionization_functions.py
+++ src/test_sessionization_functions.py
@@ -7,7 +7,7 @@ Created on Thu Apr  5 13:28:23 2018
 import unittest
 import datetime
 from sessionization import check_field_length, convert_datetime
-from sessionization import convert_unique_doc_request
+from sessionization import convert_unique_doc_request, ip_lapsed
 
 class TestFactorial(unittest.TestCase):
     """
@@ -39,6 +39,20 @@ class TestFactorial(unittest.TestCase):
         # properly formatted string returns datetime object
         is_true=isinstance(convert_unique_doc_request(tlt,0),str)
         self.assertTrue(is_true)
+        
+    # test 4
+    def test_ip_lapsed(self):
+        gp=3#grace period
+        ct=10 #current new time
+        #tuple
+        st=[("ip1",4,'s'),("ip1",5,'s'),("ip1",6,'s')] #session should end
+        sf1=[("ip2",4,'s'),("ip2",6,'s'),("ip2",8,'s')]
+        sf2=[("ip2",6,'s'),("ip2",7,'s'),("ip2",8,'s')]
+        tups=st+sf1+sf2
+        ip_lapsed(tups,gp,ct)=["ip1"]
+        
+        
+    
 
 
 
